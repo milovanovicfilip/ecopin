56256ee95f2c51c4fe49b85d0e48abd4c3870041
milovanovicfilip <milovanovic8filip@gmail.com>
2025-06-08 13:43:07 +0200

    EK-61 add deploy yml files for all enviroments, add Dockerfile for frontend, and docker-compose.yml file

--- spletno/frontend1/my-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import UserDashboard from './pages/UserDashboard';
-
-const App: React.FC = () => {
-  return <UserDashboard />;
-};
-
-export default App;
